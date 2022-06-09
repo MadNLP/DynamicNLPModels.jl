04e00d4572cc93dc6e973c8d67476db6f6d36a9a
dlcole3 <dlcole3@wisc.edu>
2022-06-09 16:29:13 -0500

    Added Qf matrix functionality, added tests for matrix

--- examples/QP_sparse_linear_mpc.jl
+++ examples/QP_sparse_linear_mpc.jl
@@ -2,13 +2,18 @@ using NLPModels, QuadraticModels, SparseArrays, Random, MadNLP, LinearAlgebra, N
 
 " Build the (sparse) H matrix for quadratic models from Q and R matrices 
  Objective function is 1/2 z^T H z = 1/2 sum(x^T Q x for i in 1:T) + 1/2 sum(u^T R u for i in 1:T)"
-function build_H(Q,R, nt)
+function build_H(Q,R, nt; Qf=[])
+
+    if size(Qf)[1] == 0
+        Qf = copy(Q)
+    end
+
     ns = size(Q)[1]
     nr = size(R)[1]
 
     H = sparse([],[],Float64[],(ns*nt + nr*(nt)), (ns*nt + nr*(nt)))
 
-    for i in 1:nt
+    for i in 1:(nt-1)
         for j in 1:ns
             for k in 1:ns
                 row_index = (i-1)*ns + k
@@ -18,6 +23,15 @@ function build_H(Q,R, nt)
             end
         end
     end
+
+    for j in 1:ns
+        for k in 1:ns
+            row_index = (nt-1)*ns + k
+            col_index = (nt-1)*ns + j
+            H[row_index, col_index] = Qf[k,j]
+        end
+    end
+
 
     for i in 1:(nt-1)
         for j in 1:nr
@@ -65,12 +79,13 @@ end
  nt is the number of time steps"
 function get_QM(Q, R, A, B, nt; c=zeros(nt*size(Q)[1] + nt*size(R)[1]),
     lvar = fill(-Inf, (nt*size(Q)[1] + nt*size(R)[1])), 
-    uvar = fill(Inf, (nt*size(Q)[1] + nt*size(R)[1]))   )
+    uvar = fill(Inf, (nt*size(Q)[1] + nt*size(R)[1])),
+    Qf = [])
 
     ns = size(Q)[1]
     nu = size(R)[1]
 
-    H = build_H(Q,R, nt)
+    H = build_H(Q,R, nt; Qf=Qf)
     A = build_A(A,B, nt)
 
     len_con = zeros(size(A)[1])
--- examples/runtests.jl
+++ examples/runtests.jl
@@ -6,19 +6,20 @@ nt = 3 # number of time steps
 ns = 2 # number of states
 nu = 1 # number of inputs
 
+# generate random Q, R, A, and B matrices
 Random.seed!(10)
-Q_org = Random.rand(ns,ns)
-Q = Q_org * transpose(Q_org) + I
-R_org   = Random.rand(nu,nu)
-R    = R_org * transpose(R_org) + I
+Q_rand = Random.rand(ns,ns)
+Q = Q_rand * transpose(Q_rand) + I
+R_rand   = Random.rand(nu,nu)
+R    = R_rand * transpose(R_rand) + I
 
-A_org = rand(ns, ns)
-A = A_org * transpose(A_org) + I
+A_rand = rand(ns, ns)
+A = A_rand * transpose(A_rand) + I
 B = rand(ns, nu)
 
-
+# generate upper and lower bounds
 lvar = rand(0:.0000001:1, (ns*nt + nu*nt))
-uvar = lvar .+ 100
+uvar = lvar .+ 10
 
 
 # build JuMP models
@@ -64,3 +65,53 @@ madnlp_ulb = madnlp(qpulb, max_iter = 50)
 @test abs(objective_value(mub) - madnlp_ub.objective)   < 1e-7
 @test abs(objective_value(mulb) - madnlp_ulb.objective) < 1e-7
 
+
+# Add Qf matrix
+Qf_rand = Random.rand(ns,ns)
+Qf = Qf_rand * transpose(Qf_rand) + I
+
+mulbf  = build_QP_JuMP_model(Q,R,A,B, nt; lvar = lvar, uvar= uvar, Qf = Qf)
+qpulbf = get_QM(Q, R, A, B, nt; lvar = lvar, uvar=uvar, Qf = Qf)
+
+# Solve new problem with Qf matrix
+optimize!(mulbf)
+ipopt_ulbf  = ipopt(qpulbf)
+madnlp_ulbf = madnlp(qpulbf)
+
+# Test results
+@test abs(objective_value(mulbf) - ipopt_ulbf.objective)  < 1e-7
+@test abs(objective_value(mulbf) - madnlp_ulbf.objective) < 1e-7
+
+
+# Test edge cases of model functions 
+nt = 50 # number of time steps
+ns = 50 # number of states
+nu = 10 # number of inputs
+
+# generate random Q, R, A, and B matrices
+Random.seed!(10)
+Q_rand = Random.rand(ns,ns)
+Q = Q_rand * transpose(Q_rand) + I
+R_rand   = Random.rand(nu,nu)
+R    = R_rand * transpose(R_rand) + I
+
+A_rand = rand(ns, ns)
+A = A_rand * transpose(A_rand) + I
+B = rand(ns, nu)
+
+# generate upper and lower bounds
+lvar = rand(0:.0000001:1, (ns*nt + nu*nt))
+uvar = lvar .+ 10
+
+Qf_rand = Random.rand(ns,ns)
+Qf = Qf_rand * transpose(Qf_rand) + I
+
+# Generate the big problems; these won't be solved because random bounds makes it likely infeasible
+mbig  = build_QP_JuMP_model(Q,R,A,B, nt; lvar=lvar, uvar=uvar, Qf=Qf)
+qpbig = get_QM(Q, R, A, B, nt; lvar=lvar, uvar=uvar, Qf=Qf)
+
+# Test if matrices formed correctly
+@test length(qpbig.data.H.rowval) == nt*ns^2 + (nt-1)*nu^2
+@test qpbig.data.c == zeros(nt*ns + nu*nt)
+@test qpbig.data.H[nt*ns + 1, nt*ns + 1] == R[1,1]
+@test qpbig.data.H[(nt-1) * ns + 1, (nt-1)*ns + 1] == Qf[1,1]
