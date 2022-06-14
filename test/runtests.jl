be33c941ae209e252ad7068e24c2f140f108478f
dlcole3 <dlcole3@wisc.edu>
2022-06-14 11:56:11 -0500

    Removed extra line of code in runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -41,8 +41,6 @@ lqplb  = LQDynamicModel(dnlplb)
 lqpub  = LQDynamicModel(dnlpub)
 lqpulb = LQDynamicModel(dnlpulb)
 
-ldmnb = LQDynamicModel()
-
 # Solve JuMP model with Ipopt
 optimize!(mnb)
 optimize!(mlb)
