b0a6f092113fdbf526bd4d9b57d1008f633c0084
dlcole3 <76058020+dlcole3@users.noreply.github.com>
2022-06-16 11:48:21 -0500

    Dc/condensed lq (#10)
    * added JuMP and QuadraticPrograms linear MPC formulations
    
    * Updated sparse LQ to start indexing at k = 0
    
    * Unbounded form of condensed formulation added to examples
    
    * Added condensed form of LQDynamicModel and modified runtests.jl
    
    * Cleaned code formatting and changed runtests.jl naming conventions

--- test/sparse_lq_test.jl
+++ test/sparse_lq_test.jl
@@ -43,36 +43,36 @@ function build_QP_JuMP_model(
     model = Model(Ipopt.Optimizer) # define model
 
 
-        @variable(model, s[NS, NN]) # define states 
-        @variable(model, u[NU, 1:(N-1)]) # define inputs
+        @variable(model, s[NS, 0:N]) # define states 
+        @variable(model, u[NU, 0:(N-1)]) # define inputs
 
 
 
     # Bound states/inputs
     if length(sl) > 0
         for i in NS
-            for j in NN
+            for j in 0:N
                 @constraint(model, s[i,j] >= sl[i])
             end
         end
     end
     if length(su) > 0
         for i in NS
-            for j in NN
+            for j in 0:N
                 @constraint(model, s[i,j] <= su[i])
             end
         end
     end
     if length(ul) > 0
         for i in NU
-            for j in 1:(N-1)
+            for j in 0:(N-1)
                 @constraint(model,  u[i,j] >= ul[i])
             end
         end
     end
     if length(uu) > 0
         for i in NU
-            for j in 1:(N-1)
+            for j in 0:(N-1)
                 @constraint(model, u[i,j] <= uu[i])
             end
         end
@@ -80,17 +80,17 @@ function build_QP_JuMP_model(
 
     if length(s0) >0
         for i in NS
-            JuMP.fix(s[i,1], s0[i])
+            JuMP.fix(s[i,0], s0[i])
         end
     end
     
 
     # Give constraints from A, B, matrices
-    @constraint(model, [t in 1:(N-1), s1 in NS], s[s1, t+1] == sum(A[s1, s2] * s[s2, t] for s2 in NS) + sum(B[s1, u1] * u[u1, t] for u1 in NU) )
+    @constraint(model, [t in 0:(N-1), s1 in NS], s[s1, t+1] == sum(A[s1, s2] * s[s2, t] for s2 in NS) + sum(B[s1, u1] * u[u1, t] for u1 in NU) )
 
     # Give objective function as xT Q x + uT R u where x is summed over T and u is summed over T-1
-    @objective(model,Min,  sum( 1/2 * Q[s1, s2]*(s[s1,t])*(s[s2,t]) for s1 in NS, s2 in NS, t in 1:(N-1)) + 
-            sum( 1/2 * R[u1,u2] * u[u1, t] * u[u2,t] for t in 1:(N-1) , u1 in NU, u2 in NU) + 
+    @objective(model,Min,  sum( 1/2 * Q[s1, s2]*(s[s1,t])*(s[s2,t]) for s1 in NS, s2 in NS, t in 0:(N-1)) + 
+            sum( 1/2 * R[u1,u2] * u[u1, t] * u[u2,t] for t in 0:(N-1) , u1 in NU, u2 in NU) + 
             sum( 1/2 * Qf[s1,s2] * s[s1,N] * s[s2, N]  for s1 in NS, s2 in NS))
 
     return model
