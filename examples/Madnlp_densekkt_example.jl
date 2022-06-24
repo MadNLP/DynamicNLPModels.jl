2228cbd278a70fe85bd3b8017b32c6b08d922c6f
fpacaud <francoispacaud8@gmail.com>
2022-06-24 10:32:32 -0500

    fix denseKKT script on the GPU

--- examples/Madnlp_densekkt_example.jl
+++ examples/Madnlp_densekkt_example.jl
@@ -10,7 +10,7 @@ function model_to_CUDA(lqdm::DenseLQDynamicModel)
     dnlp   = dynamic_data_to_CUDA(dynamic_data)
     data   = data_to_CUDA(data)
     dense_blocks = dense_blocks_to_CUDA(blocks)
-    meta   = meta_to_CUDA(meta) 
+    meta   = meta_to_CUDA(meta)
 
     DenseLQDynamicModel(
         meta,
@@ -57,7 +57,7 @@ function dynamic_data_to_CUDA(dnlp::LQDynamicData)
     LinearAlgebra.copyto!(suc, dnlp.su)
 
 
-    LQDynamicData(s0c, Ac, Bc, Qc, Rc, dnlp.N; Qf = Qfc, S = Sc,  
+    LQDynamicData(s0c, Ac, Bc, Qc, Rc, dnlp.N; Qf = Qfc, S = Sc,
     E = Ec, F = Fc, K = Kc, sl = slc, su = suc, ul = ulc, uu = uuc, gl = glc, gu = guc
     )
 end
@@ -73,7 +73,7 @@ function data_to_CUDA(data::QuadraticModels.QPData)
 
 
     QuadraticModels.QPData(
-        data.c0, 
+        data.c0,
         c,
         H,
         J
@@ -88,17 +88,17 @@ function dense_blocks_to_CUDA(db::Block_Matrices)
     F  = CuArray{Float64}(undef, size(db.F))
     gl = CuArray{Float64}(undef, size(db.gl))
     gu = CuArray{Float64}(undef, size(db.gu))
-    
+
     LinearAlgebra.copyto!(A, db.A)
     LinearAlgebra.copyto!(B, db.B)
     LinearAlgebra.copyto!(E, db.E)
     LinearAlgebra.copyto!(F, db.F)
     LinearAlgebra.copyto!(gl, db.gl)
     LinearAlgebra.copyto!(gu, db.gu)
-    
+
     Block_Matrices(A, B, db.Q, db.R, db.S, db.K, E, F, gl, gu)
 end
-    
+
 function meta_to_CUDA(meta::NLPModels.NLPModelMeta)
     x0   = CuVector{Float64}(undef, length(meta.x0))
     lvar = CuVector{Float64}(undef, length(meta.lvar))
@@ -115,8 +115,8 @@ function meta_to_CUDA(meta::NLPModels.NLPModelMeta)
     NLPModels.NLPModelMeta(
         meta.nvar,
         x0   = x0,
-        lvar = lvar, 
-        uvar = uvar, 
+        lvar = lvar,
+        uvar = uvar,
         ncon = meta.ncon,
         lcon = lcon,
         ucon = ucon,
@@ -131,15 +131,13 @@ end
 function MadNLP.jac_dense!(nlp::DenseLQDynamicModel{T, V, M1, M2, M3}, x, jac) where {T, V, M1<: Matrix, M2 <: Matrix, M3 <: Matrix}
     NLPModels.increment!(nlp, :neval_jac)
     J = nlp.data.A
-    
-    jac .= J
+    copyto!(jac, J)
 end
 
 function MadNLP.hess_dense!(nlp::DenseLQDynamicModel{T, V, M1, M2, M3}, x, w1l, hess; obj_weight = 1.0) where {T, V, M1<: Matrix, M2 <: Matrix, M3 <: Matrix}
     NLPModels.increment!(nlp, :neval_hess)
     H = nlp.data.H
-
-    hess .= H
+    copyto!(hess, H)
 end
 
 
@@ -179,24 +177,36 @@ K = rand(nu, ns)
 
 
 
-
 # Test with upper and lower bounds
 dnlp        = LQDynamicData(s0, A, B, Q, R, N; sl = sl, ul = ul, su = su, uu = uu, K = K, S = S, E = E, F = F, gl = gl, gu = gu, Qf = Qf)
 lq_sparse   = SparseLQDynamicModel(dnlp)
 lq_dense = DenseLQDynamicModel(dnlp)
 
 
-lqdm_CUDA = model_to_CUDA(lq_dense)
+# FP: there is no need to move all the data on the GPU,
+# MadNLP takes care of everything automatically so just passing lq_dense works.
+#
+# HOWEVER, for performance, we may want to instantiate the Hessian and the Jacobian
+# arrays on the GPU, like you did in lqdm_cuda. But only these two arrays
+# (and not the meta info) as MadNLP is still a CPU-based IPM solver and
+# requires the model's data to be instantiated on the CPU.
+#
+# If the model is implemented entirely on the GPU, we usually use a bridge
+# to pass the data on the CPU to MadNLP.
+#
+# lqdm_CUDA = model_to_CUDA(lq_dense)
 
 madnlp_options = Dict{Symbol, Any}(
         :kkt_system=>MadNLP.DENSE_KKT_SYSTEM,
         :linear_solver=>MadNLPLapackGPU,
-        :print_level=>MadNLP.ERROR,
+        :print_level=>MadNLP.DEBUG,
+        :jacobian_constant=>true,
+        :hessian_constant=>true,
 )
 
 
 TKKTGPU = MadNLP.DenseKKTSystem{Float64, CuVector{Float64}, CuMatrix{Float64}}
 opt = MadNLP.Options(; madnlp_options...)
     # Instantiate Solver with KKT on the GPU
-d_ips = MadNLP.InteriorPointSolver{TKKTGPU}(lqdm_CUDA, opt; option_linear_solver=copy(madnlp_options))
+d_ips = MadNLP.InteriorPointSolver{TKKTGPU}(lq_dense, opt; option_linear_solver=copy(madnlp_options))
 MadNLP.optimize!(d_ips)
